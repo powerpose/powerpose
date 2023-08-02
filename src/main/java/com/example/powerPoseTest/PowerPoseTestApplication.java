b89d1b2a8fec2a5c85562d210c38c889529a1896
jhs60110@naver.com <jhs60110@naver.com>
2023-08-02 13:36:54 +0900

    first commit

---
+++ src/main/java/com/example/powerPoseTest/PowerPoseTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.powerPoseTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerPoseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerPoseTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/powerPoseTest/PowerPoseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.powerPoseTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerPoseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
