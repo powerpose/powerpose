51548d04769006f1234d2367d285e427ea73ca8f
wxxk <dwde22@gmail.com>
2023-08-02 18:35:45 +0900

    상욱작업

---
+++ src/main/java/com/example/powerPoseTest/notice/model/Notice.java
@@ -0,0 +1,10 @@
+package com.example.powerPoseTest.notice.model;
+
+import lombok.Data;
+
+@Data
+public class Notice {
+	private int noticeId;
+	private String title;
+	private String content;
+}
---
+++ src/main/java/com/example/powerPoseTest/notice/service/INoticeService.java
@@ -0,0 +1,13 @@
+package com.example.powerPoseTest.notice.service;
+
+import java.util.List;
+
+import com.example.powerPoseTest.notice.model.Notice;
+
+public interface INoticeService {
+	List<Notice> selectNoticeList();
+	Notice selectNotice(int NoticeId);
+	void insertNotice(Notice notice);
+	void updtaeNotice(Notice notice);
+	void deleteNotice(int NoticeId);	
+}
